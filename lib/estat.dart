4ddf421a6b399e8c705879bb38d7fcf12391de5c
krypticallusion <krypticallusion@gmail.com>
2020-02-11 17:56:52 +0530

    This seems neater and effective
    Signed-off-by: krypticallusion <krypticallusion@gmail.com>

--- lib/estat.dart
+++ lib/estat.dart
@@ -1,21 +1,26 @@
 library estat;
 
 import 'dart:async' show Stream, StreamController, StreamTransformer;
+
 import 'package:flutter/material.dart';
 
 class StateContainer<T> {
   final _controller = StreamController<T>.broadcast();
-  Stream<T> get stream => _controller.stream;
+
+  Stream<T> get stream =>
+      _controller.stream.transform(StreamTransformer.fromHandlers(
+        handleData: transformer,
+      ));
+
   Function(T) get addInStream => _controller.sink.add;
+
   Sink<T> get sink => _controller.sink;
+  void Function(T value, Sink sink) transformer;
 
-  addTransformer({@required void Function(T value, Sink sink) transformer}) {
-    stream.transform(StreamTransformer.fromHandlers(
-      handleData: transformer,
-    ));
-  }
+  StateContainer({this.transformer});
 
-  stateListener({@required Widget Function(BuildContext context, T object) child}) {
+  stateListener(
+      {@required Widget Function(BuildContext context, T object) child}) {
     return StreamBuilder(
       stream: this.stream,
       builder: (BuildContext context, AsyncSnapshot<T> snapshot) {
